8a3e53a3f02c2c79b13de9e19ac3bf1e87ac5fb5
Mehmet Eren Özgün <eren.ozgun@ug.bilkent.edu.tr>
2022-04-22 00:00:29 +0300

    datdatadt

--- core/src/com/cs102/game/LastRemaindersOfThePandemic.java
+++ core/src/com/cs102/game/LastRemaindersOfThePandemic.java
@@ -3,6 +3,10 @@ package com.cs102.game;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
@@ -15,14 +19,36 @@ import java.util.EnumMap;
 public class LastRemaindersOfThePandemic extends Game {
 	private static final String TAG = LastRemaindersOfThePandemic.class.getSimpleName();
 	private EnumMap<ScreenType, Screen> screenCache;
-
-
+	private World world;
+	private FitViewport screenViewport;
+	private Box2DDebugRenderer box2DDebugRenderer;
+	public static final short BIT_CIRCLE = 1 << 0;
+	public static final short BIT_BOX = 1 << 1;
+	public static final short BIT_GROUND = 1 << 2;
 	public void create () {
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
+		Box2D.init();
+		world = new World(new Vector2(0, 9.8f), true);
+		box2DDebugRenderer = new Box2DDebugRenderer();
+		screenViewport = new FitViewport(4, 9);
 		screenCache = new EnumMap<ScreenType, Screen>(ScreenType.class);
 		setScreen(ScreenType.MENU);
 
 	}
+
+	public World getWorld() {
+		return world;
+	}
+
+	public FitViewport getScreenViewport() {
+		return screenViewport;
+	}
+
+	public Box2DDebugRenderer getBox2DDebugRenderer() {
+		return box2DDebugRenderer;
+	}
+
 	public void render () {
 		super.render();
 	}
@@ -45,4 +71,10 @@ public class LastRemaindersOfThePandemic extends Game {
 		}
 	}
 
+	@Override
+	public void dispose() {
+		super.dispose();
+		box2DDebugRenderer.dispose();
+		world.dispose();
+	}
 }
--- core/src/com/cs102/game/screens/AbstractScreen.java
+++ core/src/com/cs102/game/screens/AbstractScreen.java
@@ -1,7 +1,20 @@
 package com.cs102.game.screens;
 
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.cs102.game.LastRemaindersOfThePandemic;
+
 public class AbstractScreen {
-
-
+    protected final LastRemaindersOfThePandemic context;
+    protected final FitViewport viewport;
+    protected final World world;
+    protected final Box2DDebugRenderer box2DDebugRenderer;
+    public AbstractScreen(LastRemaindersOfThePandemic context) {
+        this.context = context;
+        viewport = context.getScreenViewport();
+        this.world = context.getWorld();
+        this.box2DDebugRenderer = context.getBox2DDebugRenderer();
+    }
 
 }
