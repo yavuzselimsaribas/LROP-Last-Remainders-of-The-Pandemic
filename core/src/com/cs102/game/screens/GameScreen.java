fe0148307d6a62805f66a6f60df45d0ecc4a83e3
Deniz Aydemir <d.aydemir@ug.bilkent.edu.tr>
2022-04-22 14:39:41 +0300

    I added Control to GameScreen and made some come changes in ContactListener

--- core/src/com/cs102/game/screens/GameScreen.java
+++ core/src/com/cs102/game/screens/GameScreen.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.physics.box2d.*;
+import com.cs102.game.Control;
 import com.cs102.game.LastRemaindersOfThePandemic;
+//import com.sun.tools.javac.jvm.Code;
 
 import static com.cs102.game.LastRemaindersOfThePandemic.*;
 
@@ -13,9 +15,74 @@ public class GameScreen extends AbstractScreen{
     //Deniz
     private final BodyDef bodyDef;
     private final FixtureDef fixtureDef;
+    private final Body player;
+
+    private Body body;
+    Control control;
     public GameScreen(LastRemaindersOfThePandemic mainGame) {
         super(mainGame);
 
+        control = new Control(1280, 720, mainGame.getGameCamera());
+        Gdx.input.setInputProcessor(control);
+
+        bodyDef = new BodyDef();
+        fixtureDef = new FixtureDef();
+
+        //create player
+        bodyDef.position.set(640f, 150f);
+        bodyDef.gravityScale = 1;
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        player = world.createBody(bodyDef);
+        player.setUserData("PLAYER");
+
+        fixtureDef.isSensor = false;
+        fixtureDef.density = 1;
+        fixtureDef.restitution = 0;
+        fixtureDef.friction = 0.2f;
+        fixtureDef.filter.categoryBits = BIT_PLAYER;
+        fixtureDef.filter.maskBits = BIT_GROUND | BIT_ITEM;
+        PolygonShape pShape = new PolygonShape();
+        pShape.setAsBox(30f, 30f);
+        fixtureDef.shape = pShape;
+        player.createFixture(fixtureDef);
+        pShape.dispose();
+
+        //create room
+        bodyDef.position.set(0,0);
+        bodyDef.gravityScale = 1;
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        body = world.createBody(bodyDef);
+        body.setUserData("GROUND");
+
+        fixtureDef.isSensor = false;
+        fixtureDef.restitution = 0;
+        fixtureDef.friction = 0.2f;
+        fixtureDef.filter.categoryBits = BIT_GROUND;
+        fixtureDef.filter.maskBits = -1;
+        final ChainShape chainShape = new ChainShape();
+        chainShape.createLoop( new float[] {30,30,30,690,1250,690,1250,30});
+        fixtureDef.shape = chainShape;
+        body.createFixture(fixtureDef);
+        chainShape.dispose();
+
+        //create item
+        bodyDef.position.set(800f, 500f);
+        bodyDef.gravityScale = 1;
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        body = world.createBody(bodyDef);
+        body.setUserData("ITEM");
+
+        fixtureDef.isSensor = true;
+        fixtureDef.restitution = 0;
+        fixtureDef.friction  =0.2f;
+        fixtureDef.filter.categoryBits = BIT_ITEM;
+        fixtureDef.filter.maskBits = BIT_PLAYER;
+        CircleShape cShape = new CircleShape();
+        cShape.setRadius(20f);
+        fixtureDef.shape = cShape;
+        body.createFixture(fixtureDef);
+        cShape.dispose();
+        /*
         //create a circle
         bodyDef = new BodyDef();
         fixtureDef = new FixtureDef();
@@ -86,6 +153,7 @@ public class GameScreen extends AbstractScreen{
         fixtureDef.shape = pShape;
         body.createFixture(fixtureDef);
         pShape.dispose();
+         */
 
     }
 
@@ -99,12 +167,40 @@ public class GameScreen extends AbstractScreen{
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            mainGame.setScreen(ScreenType.LOADING);
+        final float speed = 75;
+        final float speedX;
+        final float speedY;
+
+        if (control.left) {
+            speedX = -1 * speed;
+        }
+        else if (control.right) {
+            speedX = speed;
+        }
+        else {
+            speedX = 0;
         }
 
+        if (control.down) {
+            speedY = -1 * speed;
+        }
+        else if (control.up) {
+            speedY = speed;
+        }
+        else {
+            speedY = 0;
+        }
+
+
+        player.applyLinearImpulse(
+                (speedX - player.getLinearVelocity().x) * player.getMass(),
+                (speedY - player.getLinearVelocity().y) * player.getMass(),
+                player.getWorldCenter().x,
+                player.getWorldCenter().y,
+                true
+        );
+
         viewport.apply(true);
-        //world.step(delta, 6, 2);
         b2DDebugRenderer.render(world, viewport.getCamera().combined);
     }
 
